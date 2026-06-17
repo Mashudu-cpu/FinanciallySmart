daf63077d54776520de566ac6658639828d71f48
MashCPU <mashudunndwambi1@gmail.com>
2026-06-17 12:22:38 +0200

    Added Expenses

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,50 +1,223 @@
-const user = JSON.parse(localStorage.getItem("user"));
+const user =
+JSON.parse(
+localStorage.getItem(
+"user"
+)
+);
 
-const loggedIn = localStorage.getItem("loggedIn");
 
-if(!loggedIn){
-    window.location.href="login.html";
+
+const loggedIn =
+localStorage.getItem(
+"loggedIn"
+);
+
+
+
+if(
+!loggedIn
+){
+
+window.location.href =
+"./login.html";
+
 }
 
-document.getElementById("userName").textContent = "Welcome " +  user.name;
 
-let expenses = JSON.parse(localStorage.getItem("expenses"))||[];
 
-const expenseList = document.getElementById("expenseList");
+document
+.getElementById(
+"userName"
+)
 
-const totalElement = document.getElementById("total");
+.textContent =
+
+"Welcome " +
+user.name;
+
+
+
+let expenses =
+
+JSON.parse(
+localStorage.getItem(
+"expenses"
+)
+)
+
+|| [];
+
+
+
+const expenseList =
+
+document
+.getElementById(
+"expenseList"
+);
+
+
+
+const totalElement =
+
+document
+.getElementById(
+"total"
+);
+
+
+
 
 function renderExpenses(){
 
-    expenseList.innerHTML="";
 
-    let total=0;
-
-    expenses.forEach(
-        function(expense, index){
-            total+=expense.amount;
+expenseList.innerHTML="";
 
 
-            const item = document.createElement("div");
 
-            item.className = "expense";
+let total=0;
 
-            item.innerHTML = ``
-        }
-    )
+
+
+expenses.forEach(
+
+function(expense,index){
+
+
+total+=expense.amount;
+
+
+
+const item=
+
+document.createElement(
+"div"
+);
+
+
+
+item.className=
+
+"expense";
+
+
+
+item.innerHTML=
+
+`
+
+<div>
+
+<h3>
+
+R${expense.amount}
+
+</h3>
+
+<p>
+
+${expense.category}
+
+</p>
+
+<small>
+
+${expense.description}
+
+</small>
+
+</div>
+
+
+
+<button
+class="delete"
+
+onclick=
+"deleteExpense(${index})">
+
+Delete
+
+</button>
+
+`;
+
+
+expenseList
+.appendChild(
+item
+);
+
 }
 
-expenses.forEach(function(expense){
-    const item = document.createElement("div");
-    item.innerHTML = `<p>
-            R${expense.amount} - ${expense.category} - ${expense.description}
-            </p>`;
 
-    list.appendChild(item);
 
-});
+);
 
-document.getElementById("logout").addEventListener("click", function(){
-    localStorage.removeItem("loggedIn");
-    window.location.href = "login.html";
-});
+
+
+totalElement.textContent=
+
+"R"+total;
+
+}
+
+
+
+function deleteExpense(index){
+
+
+expenses.splice(
+index,
+1
+);
+
+
+
+localStorage.setItem(
+
+"expenses",
+
+JSON.stringify(
+expenses
+)
+
+);
+
+
+
+renderExpenses();
+
+}
+
+
+
+renderExpenses();
+
+
+
+
+document
+.getElementById(
+"logout"
+)
+
+.addEventListener(
+
+"click",
+
+function(){
+
+
+localStorage.removeItem(
+"loggedIn"
+);
+
+
+
+window.location.href=
+"./login.html";
+
+}
+
+);
